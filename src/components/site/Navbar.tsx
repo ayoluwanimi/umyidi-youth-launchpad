6e45d7e0e74b17fcac4e56aff446bf4f1899926a
ayoluwanimi <66927901+ayoluwanimi@users.noreply.github.com>
2025-08-08 17:00:12 -0700

    k

---
+++ src/components/site/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav>
+      <div className="navbar">
+        {/* Add your navbar content here */}
+        <h1>Site Navigation</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
